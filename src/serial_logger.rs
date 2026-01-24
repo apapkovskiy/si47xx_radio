a806d0daa3f2f17261cb78fb8966ab8b88c2ba86
Alexey Papkovskiy <alexey.papkovskiy@konux.de>
2026-01-24 22:21:12 +0100

    Feat: Make serial logger include timestamps and file names and colors

--- src/serial_logger.rs
+++ src/serial_logger.rs
@@ -1,12 +1,23 @@
 use log::{Record, Level, Metadata, SetLoggerError, LevelFilter};
 use core::fmt::Write as _;
+use embassy_time::Instant;
 use crate::console;
+use crate::console::console_colors::{RESET, RED, WHITE, YELLOW};
 
 struct SerialLogger;
 
 impl SerialLogger {
     pub const fn new() -> Self {
         Self
+    }
+    pub const fn get_level_color(level: Level) -> &'static core::fmt::Arguments<'static> {
+        match level {
+            Level::Error => &RED,
+            Level::Warn => &YELLOW,
+            Level::Info => &WHITE,
+            Level::Debug => &WHITE,
+            Level::Trace => &WHITE,
+        }
     }
 }
 
@@ -17,7 +28,9 @@ impl log::Log for SerialLogger {
 
     fn log(&self, record: &Record) {
         if self.enabled(record.metadata()) {
-            let _ = write!(console::stdout_get(), "{}\r\n", record.args());
+            let seconds = Instant::now().as_millis();
+            let level_color = SerialLogger::get_level_color(record.level());
+            let _ = write!(console::stdout_get(), "{level_color}[{:012}] <{}> {}: {}{RESET}\r\n", seconds, record.level(), record.file().unwrap_or("unknown"), record.args());
         }
     }
     fn flush(&self) {}
