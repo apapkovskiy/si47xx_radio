e2790a36c4283a89aaab651c984216b760bbebb2
Alexey Papkovskiy <alexey.papkovskiy@konux.de>
2026-01-24 22:22:11 +0100

    Feat: Add notification channel

--- src/events.rs
+++ src/events.rs
@@ -1,5 +1,8 @@
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::channel::Channel;
+use embassy_sync::pubsub::{PubSubChannel, Subscriber, Publisher};
+
+use si473x::{Si47xxTuneStatus, Si47xxRevision};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum SystemEvent {
@@ -16,6 +19,19 @@ pub enum SystemEvent {
     RadioVolumeSet(u8),
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum SystemNotify {
+    TuneStatus(Si47xxTuneStatus),
+    RevisionInfo(Si47xxRevision),
+    RadioFmOn,
+    RadioAmOn,
+    RadioOff,
+    RadioMute,
+    RadioUnmute,
+    VolumeChanged(u8),
+}
+
+static NOTIFICATION_CHANNEL: PubSubChannel<ThreadModeRawMutex, SystemNotify, 4, 4, 2> = PubSubChannel::new();
 static EVENT_CHANNEL: Channel<ThreadModeRawMutex, SystemEvent, 1> = Channel::new();
 
 pub async fn event_send(state: SystemEvent) {
@@ -28,4 +44,12 @@ pub fn event_try_send(state: SystemEvent) {
 
 pub async fn event_receive() -> SystemEvent {
     EVENT_CHANNEL.receive().await
+}
+
+pub fn notify_subscriber<'a>() -> Result<Subscriber<'a, ThreadModeRawMutex, SystemNotify, 4, 4, 2>, embassy_sync::pubsub::Error> {
+    NOTIFICATION_CHANNEL.subscriber()
+}
+
+pub fn notify_publisher<'a>() -> Result<Publisher<'a, ThreadModeRawMutex, SystemNotify, 4, 4, 2>, embassy_sync::pubsub::Error> {
+    NOTIFICATION_CHANNEL.publisher()
 }
